a6f8d2b1a1972ad65de7f6d3c3e3dae8fab2f142
ajian2002 <37119488+ajian2002@users.noreply.github.com>
2020-03-15 13:06:00 +0800

    update post.php
    内容回复可见

--- post.php
+++ post.php
@@ -1,3 +1,10 @@
+<style>
+.reply2view {
+    background: #f8f8f8;
+    padding: 3px;
+    position: relative;   
+}
+</style>
 <?php if(!defined('__TYPECHO_ROOT_DIR__')) exit; ?>
 <?php $this -> need('header.php'); ?>
 
@@ -19,13 +26,30 @@
 <?php if(!empty($this -> options -> post_meta) && in_array('show_comments', $this -> options -> post_meta)): ?>
                 <span class="comments"><?php $this -> commentsNum(); ?></span>
 <?php endif; ?>
+<?php if(!empty($this -> options -> post_meta) && in_array('show_view', $this -> options -> post_meta)): ?>
+                <span class="view"><?php Single::get_post_view($this) ?></span>
+<?php endif; ?>
             </div>
         </section>
         <article class="post-content">
 <?php if(time() - $this -> modified >= 15552000): ?>
             <blockquote>这篇文章上次修改于 <?php echo ceil((time() - $this -> modified) / 86400) ?> 天前，可能其部分内容已经发生变化，如有疑问可询问作者。</blockquote>
 <?php endif ?>
-            <?php $this -> content(); ?>
+           <?php
+$db = Typecho_Db::get();
+$sql = $db->select()->from('table.comments')
+    ->where('cid = ?',$this->cid)
+    ->where('mail = ?', $this->remember('mail',true))
+    ->limit(1);
+$result = $db->fetchAll($sql);
+if($this->user->hasLogin() || $result) {
+    $content = preg_replace("/\[hide\](.*?)\[\/hide\]/sm",'<div class="reply2view"><b>本贴隐藏的内容：</b><br><font color="#dc143c">$1</font></div>',$this->content);
+}
+else{
+    $content = preg_replace("/\[hide\](.*?)\[\/hide\]/sm",'<div class="reply2view">此处内容已隐藏，回复可见',$this->content);
+}
+echo $content 
+?>
         </article>
         <section class="post-near">
             <ul>
